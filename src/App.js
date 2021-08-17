8aef5f72f93fde736c9949dfa93fa79bdb6b6e7a
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-08-18 01:15:09 +0600

    React Router Example Added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,51 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import './assets/style.css';
-import ChildComponent from './components/ChildComponent';
-import TaskList from './components/TaskList';
-import Counter from './components/Counter';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import About from './components/About';
+import Contact from './components/Contact';
+import Home from './components/Home';
 
 function App() {
-
-  const [name, setName] = useState("Maniruzzaman Akash");
-
-  const [address, setAddress] = useState({
-    city: 'Dhaka',
-    area: 'Dhaka-1212'
-  })
-
-  const anotherFunc = (val) => {
-    setName(val);
-  }
-
   return (
-    <div className="App">
-      <h2 className="name">Welcome to React JS</h2>
-      {/* <ChildComponent name={name} address={address} setName={(val) => anotherFunc(val)} /> */}
-      <TaskList />
-      <Counter />
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+        </Switch>
+
+        {/* <h2 className="name">Welcome to React JS</h2> */}
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+    return ( 
+        <div>
+            <h2>Contact Us</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ipsam maiores inventore qui tempora quam cum nobis, facere non nisi, incidunt enim expedita dolorum, iure reprehenderit praesentium quaerat consequuntur similique!</p>
+        </div>
+     );
+}
+ 
+export default Contact;
